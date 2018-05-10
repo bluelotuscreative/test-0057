00a0c9e97197f2e585b731631cf1aa1c28dbf28d
oYYo <gabe@oYYos-iMac.local>
2018-05-09 20:13:26 -0700

    added tour of heroes app

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice', superpower: 'Empowerment', status: 'Veteran' },
+  { id: 12, name: 'Narco', superpower: 'Empowerment', status: 'Veteran' },
+  { id: 13, name: 'Bombasto', superpower: 'Empowerment', status: 'Veteran' },
+  { id: 14, name: 'Celeritas', superpower: 'Empowerment', status: 'Veteran' },
+  { id: 15, name: 'Magneta', superpower: 'Empowerment', status: 'Veteran' },
+  { id: 16, name: 'RubberMan', superpower: 'Empowerment', status: 'Veteran' },
+  { id: 17, name: 'Dynama', superpower: 'Empowerment', status: 'Veteran' },
+  { id: 18, name: 'Dr IQ', superpower: 'Empowerment', status: 'Veteran' },
+  { id: 19, name: 'Magma', superpower: 'Empowerment', status: 'Veteran' },
+  { id: 20, name: 'Tornado', superpower: 'Empowerment', status: 'Veteran' }
+];
